19e038cc03b339cf668218c91efbeebdc5c7e98f
mhardiansyah <mhardiansyah5766@gmail.com>
2025-09-04 10:00:59 +0700

    config

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+Future main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await GetStorage.init();
   runApp(
     GetMaterialApp(
       title: "Application",
