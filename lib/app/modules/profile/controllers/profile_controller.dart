0defdab04ab67c89337253de2c928ea5d909dc96
mhardiansyah <mhardiansyah5766@gmail.com>
2025-11-05 12:04:10 +0700

    test

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,16 +1,35 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class ProfileController extends GetxController {
-  var name = 'Faqih Abqory'.obs;
-  var email = 'SitiM@gmail.com'.obs;
-  var password = '********'.obs;
+  final box = GetStorage();
+
+  var name = ''.obs;
+  var email = ''.obs;
+  var username = ''.obs;
+  var password = ''.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    loadUserData();
+  }
+
+  void loadUserData() {
+    name.value = box.read('name') ?? 'Unknown User';
+    email.value = box.read('email') ?? 'No Email';
+    username.value = box.read('username') ?? 'No Username';
+    password.value = '********';
+  }
 
   void logout() {
-    // Aksi logout di sini
+    box.erase(); // hapus semua data login
     Get.snackbar(
       'Logout',
       'Anda telah logout',
       snackPosition: SnackPosition.BOTTOM,
     );
+    // Arahkan ke halaman login
+    // Get.offAllNamed(Routes.LOGIN);
   }
 }
